ff5c0cb2369304754816f25eb7cdfcb3fc9a3cae
Zelal EizAldeen <zelalezaldeen@gmail.com>
2024-05-29 14:11:42 +0300

    Updates

--- index.js
+++ index.js
@@ -1023,4 +1023,4 @@ const targetElements = [
 
 },{"@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}]},["ShInH","8lqZg"], "8lqZg", "parcelRequire6aa4")
 
-//# sourceMappingURL=index.975ef6c8.js.map
+//# sourceMappingURL=index.js.map
